39a2ccfa6135a1956bb5e0c4d5a3791b44383610
gbeford <gbeford@gmail.com>
2017-06-19 13:47:10 -0400

    Add All Form Fields

--- src/app/match-scoring/match-scoring.component.ts
+++ src/app/match-scoring/match-scoring.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./match-scoring.component.css']
 })
 export class MatchScoringComponent implements OnInit {
-
+  title = 'Match';
   constructor() { }
 
   ngOnInit() {
