e8ee462b0ccfe8136af347177605134516b09365
gbeford <gbeford@gmail.com>
2017-06-19 18:40:35 -0400

    add Radio buttons

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -3,12 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatchScoringComponent } from 'app/match-scoring/match-scoring.component';
 import { PageNotFoundComponent } from 'app/page-not-found/page-not-found.component';
+import { TeamListingComponent } from 'app/team-listing/team-listing.component';
+
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forRoot([
       { path: 'match', component: MatchScoringComponent },
+      { path: 'teamList', component: TeamListingComponent },
       { path: '**', component: PageNotFoundComponent }
     ]),
   ],
