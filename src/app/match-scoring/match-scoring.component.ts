16e57ee7ea49a4e7c6045958ce7b76e3f45c5c5b
gbeford <gbeford@gmail.com>
2017-06-20 11:52:14 -0400

    add match service

--- src/app/match-scoring/match-scoring.component.ts
+++ src/app/match-scoring/match-scoring.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MatchService } from './match.service';
 
 @Component({
   selector: 'app-match-scoring',
   templateUrl: './match-scoring.component.html',
-  styleUrls: ['./match-scoring.component.css']
+  styleUrls: ['./match-scoring.component.css'],
+  providers: [MatchService]
 })
+
+
 export class MatchScoringComponent implements OnInit {
   title = 'Match';
-  constructor() { }
+
+  constructor(private _matchService: MatchService) { }
+
 
   ngOnInit() {
+    this.title = this._matchService.someMethod();
   }
 
 }
