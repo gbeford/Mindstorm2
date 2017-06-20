ccb90e49836c8af9d329a558d178ea1f9d35117c
gbeford <gbeford@gmail.com>
2017-06-20 13:07:19 -0400

    add shell for api calls

--- src/app/match-scoring/match-scoring.component.ts
+++ src/app/match-scoring/match-scoring.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+
 import { MatchService } from './match.service';
 
 @Component({
   selector: 'app-match-scoring',
+  providers: [MatchService],
   templateUrl: './match-scoring.component.html',
-  styleUrls: ['./match-scoring.component.css'],
-  providers: [MatchService]
+  styleUrls: ['./match-scoring.component.css']
+ 
 })
 
 
 export class MatchScoringComponent implements OnInit {
   title = 'Match';
+ public matchItem = {};
+
 
   constructor(private _matchService: MatchService) { }
+ 
+  matchData = [];
 
 
   ngOnInit() {
-    this.title = this._matchService.someMethod();
+   // this.title = this._matchService.someMethod();
   }
+
+  // saveMatch() {
+  //   this._matchService.getmatchData().subscribe(data => this.matchData = data);
+  // }
+
+  // private getAllItems(): void {
+  //   this._matchService
+  //     .GetAll()
+  //     .subscribe((data: matchItem[]) => this.myItems = data,
+  //     error => console.log(error),
+  //     () => console.log('Get all Items complete'));
+  // }
 
 }
